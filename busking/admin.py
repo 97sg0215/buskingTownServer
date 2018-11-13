ffb6adce8e5d2b743e861154148a3426194d9a5c
soogyeong <97sg0215@gmail.com>
2018-11-14 04:23:32 +0900

    길거리 예약 위도 경도 필드 추가

--- busking/admin.py
+++ busking/admin.py
@@ -11,8 +11,12 @@ class CoinAdmin(admin.ModelAdmin):
     list_display = ('supportCoin_id','busker','user','coin_amount', 'date_created', 'coin_balance')
 
 class RoadConcertAdmin(admin.ModelAdmin):
-    list_display = ('road_concert_id', 'busker', 'road_address', 'road_name', 'road_concert_date', 'road_concert_start_time', 'road_concert_end_time')
+    list_display = ('road_concert_id', 'busker', 'road_address','road_lon','road_lat', 'road_name', 'road_concert_date', 'road_concert_start_time', 'road_concert_end_time')
+
+# class FollowerStatics(admin.ModelAdmin):
+#     list_display = ('busker', 'follower_count_statistics_date')
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(supportCoin, CoinAdmin)
 admin.site.register(RoadConcert, RoadConcertAdmin)
+# admin.site.register(BuskerFollowerStatistics, FollowerStatics)
